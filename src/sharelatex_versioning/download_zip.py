0aac30e5e9b2a5d8fa6edd8b0738addf4fa3787e
Patrick Stoeckle <patrick.stoeckle@tum.de>
2020-10-28 11:15:39 +0100

    :sparkles: Hash zip file content

--- src/sharelatex_versioning/download_zip.py
+++ src/sharelatex_versioning/download_zip.py
@@ -3,13 +3,15 @@ Download zip.
 """
 from fnmatch import fnmatch
 from functools import partial
+from hashlib import sha1
 from json import load
 from logging import INFO, basicConfig, getLogger
 from os import chmod, path, remove, sep, walk
+from os.path import isfile, join
 from stat import S_IRUSR, S_IWUSR
 from subprocess import call
 from tempfile import gettempdir
-from typing import List
+from typing import List, Optional
 from zipfile import ZipFile
 
 from requests import Session
@@ -52,6 +54,9 @@ def download_zip_implementation(
         with open(in_file) as f_read:
             data: Configuration = load(f_read)
         zip_file_location = _download_zip_file(data["project_id"], data["share_id"])
+        if not _are_there_new_changes(working_dir, zip_file_location):
+            remove(zip_file_location)
+            return
         line_matcher = _create_line_matchers(
             path.basename(in_file), white_list, working_dir
         )
@@ -81,6 +86,68 @@ def download_zip_implementation(
         _file_deletion(zip_file_location, True)
     else:
         _LOGGER.critical("Error: Config was empty!")
+
+
+def hash_file(file_name: str) -> str:
+    """
+
+    :param file_name:
+    :return:
+    """
+    if not isfile(file_name):
+        return ""
+    current_sha = sha1()
+    with open(file_name, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            current_sha.update(chunk)
+    return current_sha.hexdigest()
+
+
+def hash_files_in_zip(zip_file: str) -> str:
+    """
+
+    Args:
+        zip_file:
+
+    Returns:
+
+    """
+    current_sha = sha1()
+    with ZipFile(zip_file) as zip_ref:
+        for name in zip_ref.namelist():
+            with zip_ref.open(name) as current_file:
+                for chunk in iter(lambda: current_file.read(4096), b""):
+                    current_sha.update(chunk)
+    hexdigest = current_sha.hexdigest()
+    _LOGGER.info(f"File {zip_file} -> {hexdigest}")
+    return hexdigest
+
+
+def _are_there_new_changes(working_dir: str, zip_file_location: str) -> bool:
+    """
+
+    Args:
+        working_dir:
+        zip_file_location:
+
+    Returns:
+
+    """
+    last_hash_file = join(working_dir, ".sharelatex_versioning")
+    last_hash: Optional[str] = None
+    if isfile(last_hash_file):
+        with open(last_hash_file) as f_read:
+            last_hash = f_read.read().strip()
+    current_hash = hash_files_in_zip(zip_file_location)
+    if last_hash is not None and current_hash == last_hash:
+        _LOGGER.info(
+            f"After hashing all files of the new zip, we got the same hash ({current_hash}) as for the last zip. No new changes..."
+        )
+        return False
+    else:
+        with open(last_hash_file, "w") as f_write:
+            f_write.write(current_hash)
+    return True
 
 
 def _replace_workdir(file_name: str, workdir: str) -> str:
