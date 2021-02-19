b4da0e10cdb54a07e8fab3cc2426e2ab509f90eb
Patrick Stoeckle <patrick.stoeckle@tum.de>
2021-02-19 14:05:03 +0100

    :sparkles: Password via cli

--- sharelatex_versioning/main.py
+++ sharelatex_versioning/main.py
@@ -3,9 +3,11 @@ Stuff to clean things.
 """
 from logging import INFO, basicConfig, getLogger
 from sys import stdout
-from typing import Optional
+from typing import Optional, Any
 
-from click import group, option
+from click import Choice, Context
+from click import Path as click_Path
+from click import echo, group, option
 
 from sharelatex_versioning import __version__
 from sharelatex_versioning.logic.download_zip import download_zip_and_extract_content
@@ -23,8 +25,18 @@ basicConfig(
 )
 
 
-def _print_version() -> None:
-    _LOGGER.info(f"sharelatex-versioning {__version__}")
+def _print_version(ctx: Context, _: Any, value: Any) -> None:
+    """
+
+    :param ctx:
+    :param _:
+    :param value:
+    :return:
+    """
+    if not value or ctx.resilient_parsing:
+        return
+    echo(__version__)
+    ctx.exit()
 
 
 @group()
@@ -71,12 +83,14 @@ def main_group() -> None:
     help="If this flag is passed, all the files which are not part of the ShareLaTeX project "
     "and not covered by .gitignore or the white_list option, are deleted.",
 )
+@option("--password", "-p", help="The LRZ password", default=None)
 @main_group.command()
 def download_zip(
     in_file: str,
     force: bool,
     white_list: Optional[str],
     working_dir: str,
+    password: Optional[str],
 ) -> None:
     """
     This command downloads your ShareLaTeX project as ZIP compressed file.
@@ -85,8 +99,7 @@ def download_zip(
     If you want, the script can also delete all the files which are no longer in your project.
     Thus, files deleted on the ShareLaTeX instance are also deleted locally.
     """
-    _print_version()
-    download_zip_and_extract_content(force, in_file, white_list, working_dir)
+    download_zip_and_extract_content(force, in_file, white_list, working_dir, password)
 
 
 if __name__ == "__main__":
