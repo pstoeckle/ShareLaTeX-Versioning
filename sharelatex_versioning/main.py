4b94d8a3dad1332ace2433c7bcc8a8f0cedece28
Patrick Stoeckle <patrick.stoeckle@tum.de>
2021-03-15 16:49:54 +0100

    :sparkles: Password handling with keyring

--- sharelatex_versioning/main.py
+++ sharelatex_versioning/main.py
@@ -3,12 +3,11 @@ Stuff to clean things.
 """
 from logging import INFO, basicConfig, getLogger
 from sys import stdout
-from typing import Optional, Any
+from typing import Any, Optional
 
-from click import Choice, Context
-from click import Path as click_Path
-from click import echo, group, option
+from click import Context, echo, group, option
 
+from password_handling import store_password
 from sharelatex_versioning import __version__
 from sharelatex_versioning.logic.download_zip import download_zip_and_extract_content
 
@@ -83,14 +82,12 @@ def main_group() -> None:
     help="If this flag is passed, all the files which are not part of the ShareLaTeX project "
     "and not covered by .gitignore or the white_list option, are deleted.",
 )
-@option("--password", "-p", help="The LRZ password", default=None)
 @main_group.command()
 def download_zip(
     in_file: str,
     force: bool,
     white_list: Optional[str],
     working_dir: str,
-    password: Optional[str],
 ) -> None:
     """
     This command downloads your ShareLaTeX project as ZIP compressed file.
@@ -99,7 +96,32 @@ def download_zip(
     If you want, the script can also delete all the files which are no longer in your project.
     Thus, files deleted on the ShareLaTeX instance are also deleted locally.
     """
-    download_zip_and_extract_content(force, in_file, white_list, working_dir, password)
+    download_zip_and_extract_content(force, in_file, white_list, working_dir)
+
+
+@option("--user_name", "-u", help="The username", prompt=True)
+@option(
+    "--password",
+    "-p",
+    prompt=True,
+    hide_input=True,
+    help="The password for the IMAP/SMTP server.",
+)
+@option(
+    "--force",
+    "-f",
+    is_flag=True,
+    default=False,
+    help="If true, we will overwrite existing passwords.",
+)
+@main_group.command()
+def store_password_in_password_manager(
+    password: str, user_name: str, force: bool
+) -> None:
+    """
+    Stores the password in the password manager.
+    """
+    store_password(force, password, user_name)
 
 
 if __name__ == "__main__":
